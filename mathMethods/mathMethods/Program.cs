1e7b5d8d83c5ad5375f6642ad4cacc6f64e8fd74
Sam Huebschwerlen <39445414+huebschwerlen@users.noreply.github.com>
2018-10-12 12:00:28 -0700

    create method that performs simple math when called

---
+++ mathMethods/mathMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to add 10 to it.");
+            int intInput = Convert.ToInt32(Console.ReadLine());
+            
+            intInput = mathClass.AddN(intInput);
+            Console.WriteLine(intInput);
+            Console.ReadKey();
+
+            intInput = mathClass.SubN(intInput);
+            Console.WriteLine(intInput);
+            Console.ReadKey();
+
+            intInput = mathClass.MultN(intInput);
+            Console.WriteLine(intInput);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ mathMethods/mathMethods/mathClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathMethods
+{
+    public class mathClass
+    {
+
+        public static int AddN(int intInput)
+        {
+            intInput = intInput + 10;
+            return intInput;
+        }
+
+        public static int SubN(int intInput)
+        {
+            intInput = intInput - 10;
+            return intInput;
+        }
+
+        public static int MultN(int intInput)
+        {
+            intInput = intInput * 10;
+            return intInput;
+        }
+
+    }
+}
