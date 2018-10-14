c1b6fc07a5dcaaec2f5d0993a5889130a1ba6539
Sam Huebschwerlen <39445414+huebschwerlen@users.noreply.github.com>
2018-10-14 15:52:27 -0700

    create method with optional param

---
+++ optionalParam/optionalParam/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalParam
+{
+    class Class1
+    {
+
+        /*
+         1. Create a class. In that class, create a method
+         that takes two integers as parameters. Make one of 
+         them optional. Have the method do a math operation 
+         and return a integer result.
+         */
+
+        public int MathOp(int int1, int int2 = 1)
+        {
+            int operation1 = int1 + int2;
+            return operation1;
+        }
+    }
+}
---
+++ optionalParam/optionalParam/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalParam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.In the Main() method of the console app, instantiate the class.
+            Class1 math1 = new Class1();
+
+            /*
+             3. Ask the user to input two numbers, one at a time. 
+             Let them know they need not enter anything for the second number.
+            */
+            
+            Console.WriteLine("Please enter a number: ");
+            int intInput1 = Convert.ToInt32(Console.ReadLine());
+            int intInput2;
+            try
+            {
+                Console.WriteLine("Optional: Enter a second number: ");
+                intInput2 = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                intInput2 = 0;
+            }
+            
+            
+            
+
+                //4.Call the method in the class, passing in the one or two numbers entered.
+                int intInput = math1.MathOp(intInput1, intInput2);
+            Console.WriteLine(intInput);
+            Console.ReadKey();
+        }
+    }
+}
