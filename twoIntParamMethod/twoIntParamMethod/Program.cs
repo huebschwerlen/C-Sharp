43a6d7c74017ef2b01a4f135bc644ae6a456f0d2
Sam Huebschwerlen <39445414+huebschwerlen@users.noreply.github.com>
2018-10-14 09:40:50 -0700

    create method with two parameters and call the method specifying param by name

---
+++ twoIntParamMethod/twoIntParamMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twoIntParamMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            voidMeth math1 = new voidMeth();
+            {
+                voidMeth.mathMeth(int1: 2, int2: 3);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+
+            //voidMeth math2 = new voidMeth(int int1 = 2, int int2 = 3);
+            //{
+            //    voidMeth.mathMeth("int1 = {0}, int2 = {1}", int1, int2);
+            //}
+            //Console.WriteLine();
+            //Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ twoIntParamMethod/twoIntParamMethod/voidMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twoIntParamMethod
+{
+    public class voidMeth
+    {
+        public static void mathMeth(int int1 = 2, int int2 = 4)
+        {
+            int1 = int1 + 50;
+            Console.WriteLine(int2);
+        }
+    }
+}
