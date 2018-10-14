82da3d77053676dd7b0af85658a8c22439abc324
Sam Huebschwerlen <39445414+huebschwerlen@users.noreply.github.com>
2018-10-14 15:00:04 -0700

    create exercise for more methods

---
+++ moreMethods/moreMethods/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moreMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             2. In the Main() method of the console app, 
+             instantiate the class and call the one method, 
+             passing in an integer, such as 12. 
+             Then display the result to the screen.
+             */
+            mathOp math1 = new mathOp();
+            int returnVal = 0;
+            returnVal = math1.methOne(12);
+            
+            Console.WriteLine(returnVal);
+            
+
+
+            /*
+             4. In the Main() method of the console app, 
+             instantiate the class and call the second method, 
+             passing in a decimal. Display the result to the screen.
+             */
+
+            mathOp math2 = new mathOp();
+            int returnVal1 = 0;
+            returnVal1 = math2.methOne(13.22m);
+
+            Console.WriteLine(returnVal1);
+            
+
+
+
+            /*
+             6. In the Main() method of the console app, 
+             instantiate the class and call the third method, 
+             passing in a string that equates to an integer. 
+             Display the result to the screen.
+             */
+
+            mathOp math3 = new mathOp();
+            int returnVal2 = 0;
+            returnVal2 = math3.methOne("2");
+
+            Console.WriteLine(returnVal2);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ moreMethods/moreMethods/mathOp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moreMethods
+{
+    public class mathOp
+    {
+        /* 
+         1. Create a class. In that class, 
+         create a method that will take in an integer, 
+         do a math operation to it and then 
+         return the answer as an integer.
+         */
+
+        public int methOne(int firstNum)
+        {
+            int intInput;
+            intInput = firstNum + 10;
+            return intInput;
+
+        }
+
+
+        /*
+        3. Add a second method to the class, 
+        with the same name, that will take in a decimal, 
+        do a different math operation to it 
+        and then return the answer as an integer.
+        */
+
+        public int methOne(decimal decNum)
+        {
+            int decInput;
+            decInput = (int)decNum * 20;
+            return decInput;
+
+        }
+
+
+        /*
+         5. Add a third method to the class, 
+         with the same name, that will take in a string, 
+         convert it to an integer if possible, 
+         do a different math operation to it and 
+         then return the answer as an integer.
+         */
+
+        public int methOne(string stringIn)
+        {
+            int stringNum = System.Convert.ToInt32(stringIn);
+            int outStringNum = stringNum - 1;
+            return outStringNum;
+            
+        }
+
+    }
+}
