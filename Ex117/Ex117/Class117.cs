7cdb36ffe1a0b7defe4708017a0b54454ac53849
Sam Huebschwerlen <39445414+huebschwerlen@users.noreply.github.com>
2018-10-15 15:38:45 -0700

    create void method with output param

---
+++ Ex117/Ex117/Class117.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex117
+{
+    public class Class117
+    {
+        //1.create void method that outputs an integer.
+        public void Method117(int inInt, out int outInt) //4.create a method with output parameters.
+        {
+            outInt = inInt / 2; //have the method divide the data passed to it by 2.
+        }
+
+        //5.overlaod a method
+        public static int Method117(int inInt1) //6.declare a class to be static.
+        {
+            int outInt1 = inInt1 / 2;
+            
+            Console.WriteLine(outInt1);
+            return outInt1;
+        }
+    }
+}
---
+++ Ex117/Ex117/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class117 Instan117 = new Class117(); //2.instantiate the class
+            int Quotient0;
+            Console.WriteLine("Enter a number to have it divided by 2: "); //3.have the user enter a number. 
+            int UserInput = Convert.ToInt32(Console.ReadLine());
+            Instan117.Method117(UserInput, out Quotient0); //call the method on that number. 
+            Console.WriteLine(Quotient0); //display the output to the screen. it should be the entered number, divided by two.
+            Console.ReadLine();
+
+
+            Class117.Method117(UserInput);
+            Console.ReadLine();
+        }
+    }
+}
