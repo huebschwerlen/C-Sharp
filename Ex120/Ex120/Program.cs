85d770a9ff10daab8adb5f15d47da1a8244ae563
Sam Huebschwerlen <39445414+huebschwerlen@users.noreply.github.com>
2018-10-16 11:41:15 -0700

    create inheritance exercise step 120

---
+++ Ex120/Ex120/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex120
+{
+    class Program
+    {
+        /*
+         4. Inside of the Main method, instantiate 
+         and initialize an Employee object with a first 
+         name of "Sample" and a last name of "Student".
+         */
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName(); //5. Call the superclass method SayName() on the Employee object.
+            Console.ReadLine();
+        }
+       
+    }
+    
+}
