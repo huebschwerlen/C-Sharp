95d14746f49f081fa8fb62c22df6ff34a353897c
Sam Huebschwerlen <39445414+huebschwerlen@users.noreply.github.com>
2018-10-17 11:30:52 -0700

    create exercise for step 129

---
+++ Ex129/Ex129/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex129
+{
+    public class Employee : Person, IQuittable
+    {
+
+        public int Id { get; set; }
+
+        void SayName()
+        {
+
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("I QUIT");
+        }
+
+        public static bool operator ==(Employee id1, Employee id2)
+        {
+            bool status = false;
+            if (id1.Id == id2.Id)
+            {
+                status = true;
+            }
+            return status;
+        }
+        public static bool operator !=(Employee id1, Employee id2)
+        {
+            bool status = false;
+            if (id1.Id == id2.Id)
+            {
+                status = true;
+            }
+            return status;
+        }
+    }
+}
---
+++ Ex129/Ex129/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex129
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student", Id = 28 };
+            employee.SayName();
+            Console.ReadLine();
+
+          
+            IQuittable employeeQuit = new Employee();
+            employeeQuit.Quit();
+            Console.ReadLine();
+
+            Employee employee1 = new Employee() { FirstName = "Jared", LastName = "Junior", Id = 28 };
+            
+           
+
+            if (employee == employee1)
+                Console.WriteLine("IDs are equal.");
+            else
+                Console.WriteLine("IDs are not equal.");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
