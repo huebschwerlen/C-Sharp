911b7a4801b9bcf38e82a731a6cb163acd1195b0
Sam Huebschwerlen <39445414+huebschwerlen@users.noreply.github.com>
2018-10-17 15:50:07 -0700

    create exercise for step 134

---
+++ Ex134/Ex134/EnumClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex134
+{
+    public class EnumClass
+    //1. Create an enum for the days of the week.
+    {
+        public enum DayOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
---
+++ Ex134/Ex134/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a day of the week: ");//2. Prompt the user to enter the current day of the week.
+            string response = Console.ReadLine();
+
+            DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), response);
+
+    
+            Console.WriteLine(response + " " + day);
+            Console.ReadLine();
+        }
+    }
+}
