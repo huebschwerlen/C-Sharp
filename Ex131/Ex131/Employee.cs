e517ee6179c1c0698ef1edd427e0bb7a0e802208
Sam Huebschwerlen <39445414+huebschwerlen@users.noreply.github.com>
2018-10-17 12:40:34 -0700

    create exercise for step 131

---
+++ Ex131/Ex131/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex131
+{
+    public class Employee<T>
+    {
+
+        public int Id { get; set; }
+
+        public List<T> Things { get; set; }
+
+        
+    }
+}
---
+++ Ex131/Ex131/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> things = new Employee<string>();
+            things.Things = new List<string> { "thingOne", "thingTwo", "thingThree" };
+
+            Employee<int> thingsInt = new Employee<int>();
+            thingsInt.Things = new List<int> { 1, 2, 3 };
+
+            for (int i = 0; i < thingsInt.Things.Count; i++)
+            {
+                Console.WriteLine(thingsInt.Things[i]);
+            }
+
+            foreach(string thing in things.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
