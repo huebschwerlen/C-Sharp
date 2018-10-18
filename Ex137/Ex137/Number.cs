1e48fa23f141c685f2df7f231c76eed551f38059
Sam Huebschwerlen <39445414+huebschwerlen@users.noreply.github.com>
2018-10-17 17:07:51 -0700

    create exercise for step 137

---
+++ Ex137/Ex137/Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex137
+{   /*
+    1. Create a struct called Number and give it 
+    the property "Amount" and have it be of data type decimal.
+    */
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Ex137/Ex137/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             2. In the Main() method, create an object of 
+             data type Number and assign an amount to it.
+             */
+            Number number1 = new Number();
+             number1.Amount = 90.01m;
+
+            Console.WriteLine(number1.Amount);//3. Print this amount to the console.
+            Console.ReadLine();
+        }
+    }
+}
